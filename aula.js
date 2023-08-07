f925467772938f53fc333a744847b199c948b197
EdgarAllandirPoy <edgar.F.A2007@gmail.com>
2023-08-07 13:55:43 -0300

    a

---
+++ aula.js
@@ -0,0 +1,19 @@
+
+// Math.ramdom gera n aleatório
+console.log(Math.random());
+
+//Gerando um n aleatorio entre 0 a 20
+console.log(Math.random() * 20 );
+console.log(Math.round(Math.random()*5));
+
+//Math.round arredonda para o n mais proximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+//Math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+//Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
